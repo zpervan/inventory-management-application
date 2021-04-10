b1f66a18642880ca2f4171b27b973f14b1db37ae
Zvonimir Pervan <z.pervan65@gmail.com>
2021-04-10 08:54:31 +0200

    general: Adjust header paths in GUILib and add example code

--- GUI/gui_runner.cpp
+++ GUI/gui_runner.cpp
@@ -4,11 +4,12 @@
 
 #include <d3d10.h>
 #include <d3d10_1.h>
-#include <imgui.h>
 #include <tchar.h>
 
-#include "imgui_impl_dx10.h"
-#include "imgui_impl_win32.h"
+/// @TODO: Avoid relative paths
+#include "../ThirdParty/DearImgui/imgui.h"
+#include "../ThirdParty/DearImgui/imgui_impl_dx10.h"
+#include "../ThirdParty/DearImgui/imgui_impl_win32.h"
 
 void Runner::Run()
 {
--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
+#include <memory>
+
 #include "Backend/DatabaseQuery/database_query.h"
+#include "GUI/gui_runner.h"
+
 int main()
 {
     DatabaseQuery dbq;
-
     dbq.Connect();
     dbq.Fetch();
+
+    std::unique_ptr<Runner> runner{new Runner};
+    runner->Run();
 
     return 0;
 }
