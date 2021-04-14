586643acea383452cb5b57779f07951d340a7f78
Zvonimir Pervan <z.pervan65@gmail.com>
2021-04-14 17:47:58 +0200

    general: Add central settings file

---
+++ GUI/application_settings.h
@@ -0,0 +1,17 @@
+#ifndef INVENTORYMANAGEMENTAPPLICATION_IMGUI_CONTENT_WINDOW_H
+#define INVENTORYMANAGEMENTAPPLICATION_IMGUI_CONTENT_WINDOW_H
+
+#include "../ThirdParty/DearImgui/imgui.h"
+#include "directx_window.h"
+
+namespace properties {
+const ImVec4 kGClearColor {0.45f, 0.55f, 0.60f, 1.00f};
+const ImGuiWindowFlags kGWindowFlags {ImGuiWindowFlags_NoTitleBar + ImGuiWindowFlags_NoResize + ImGuiWindowFlags_NoCollapse + ImGuiWindowFlags_NoMove};
+}
+
+namespace main_menu {
+const ImVec2 kGSize {static_cast<float>(GetScreenResolution().first) * 0.15f, static_cast<float>(GetScreenResolution().second)};
+const ImVec2 kGPosition{0.0, 20.0};
+}
+
+#endif //INVENTORYMANAGEMENTAPPLICATION_IMGUI_CONTENT_WINDOW_H
