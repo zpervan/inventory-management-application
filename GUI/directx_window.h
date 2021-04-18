3f79f22d04a550b7dfba7a9de973e518c03818d3
Zvonimir Pervan <z.pervan65@gmail.com>
2021-04-18 17:57:46 +0200

    refactor: Make DirectX functions static and minor code refactor

--- GUI/directx_window.h
+++ GUI/directx_window.h
@@ -12,13 +12,13 @@
 #include "../ThirdParty/DearImgui/imgui_impl_win32.h"
 
 // Forward declarations
-bool CreateDeviceD3D(HWND hWnd);
-void CleanupDeviceD3D();
-void CreateRenderTarget();
-void CleanupRenderTarget();
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+static bool CreateDeviceD3D(HWND hWnd);
+static void CleanupDeviceD3D();
+static void CreateRenderTarget();
+static void CleanupRenderTarget();
+static LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-std::pair<int, int> GetScreenResolution();
+static std::pair<int, int> GetScreenResolution();
 
 namespace window
 {
@@ -36,7 +36,7 @@ static std::pair<int, int> g_window_size{GetScreenResolution()};
 // Forward declare message handler from imgui_impl_win32.cpp
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-inline void CreateDirectXWindow(const std::string& application_title)
+static void CreateDirectXWindow(const std::string& application_title)
 {
     window::g_wc = WNDCLASSEX{sizeof(WNDCLASSEX),
                               CS_CLASSDC,
