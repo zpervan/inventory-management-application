4c4ca74bb8f82f32f2a0fe11828bc5ad31f04cc5
Zvonimir Pervan <z.pervan65@gmail.com>
2021-04-18 17:56:30 +0200

    minor: Adjust main menu origin position

--- GUI/application_settings.h
+++ GUI/application_settings.h
@@ -11,7 +11,8 @@ const ImGuiWindowFlags kGWindowFlags {ImGuiWindowFlags_NoTitleBar + ImGuiWindowF
 
 namespace main_menu {
 const ImVec2 kGSize {static_cast<float>(GetScreenResolution().first) * 0.15f, static_cast<float>(GetScreenResolution().second)};
-const ImVec2 kGPosition{0.0, 20.0};
+/// @TODO: Make main menu position calculated dynamically (no hardcoded values)
+const ImVec2 kGPosition{0.0, 22}; // y = font size (16) + frame padding (6)
 }
 
 #endif //INVENTORYMANAGEMENTAPPLICATION_IMGUI_CONTENT_WINDOW_H
